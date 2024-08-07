d007607974cb5ac4df6e15ebbfad6aa9062b5377
Josh Prior <joshprior83@gmail.com>
2024-08-07 10:53:06 -0500

    Add reset button
    Temp reset button until I implement full down refresh

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabs } from "expo-router";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Game } from "@/components/Game";
+import { View, StyleSheet, Pressable, Text } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const [id, setId] = useState("123");
   return (
     // <Tabs
     //   screenOptions={{
@@ -33,7 +34,39 @@ export default function TabLayout() {
     //   />
     // </Tabs>
     <React.StrictMode>
-      <Game />
+      <Game key={id} />
+      <View style={styles.container}>
+        <Pressable
+          style={styles.resetButton}
+          onPress={() => {
+            setId(Math.random().toString());
+          }}
+        >
+          <Text style={styles.buttonText}>Reset</Text>
+        </Pressable>
+      </View>
     </React.StrictMode>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    //backgroundColor: "orange",
+    flex: 0.1,
+    justifyContent: "center",
+  },
+  resetButton: {
+    borderWidth: 0,
+    borderRadius: 20,
+    backgroundColor: "blue",
+    marginHorizontal: 40,
+    paddingVertical: 5,
+    justifyContent: "flex-end",
+  },
+  buttonText: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "white",
+    zIndex: 1001,
+  },
+});
